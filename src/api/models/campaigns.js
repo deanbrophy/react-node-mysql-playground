5a838c229de7ef387d8199ac5d7a5baf42e51de9
Dean Brophy <dbrophy@ihsmarkit.com>
2018-03-02 18:01:55 -0700

    Will this work once checked into git

--- src/api/models/campaigns.js
+++ src/api/models/campaigns.js
@@ -1,9 +1,16 @@
 'use strict';
+
+const Sequelize = require('sequelize');
+const { data} = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
   var Campaigns = sequelize.define('Campaigns', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+    id: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
     name: DataTypes.STRING,
-    url: DataTypes.STRING
+    url: DataTypes.STRING,
+    createdAt: {type: Sequelize.DATE, defaultValue: Sequelize.NOW},
+    updatedAt: {type: Sequelize.DATE, defaultValue: Sequelize.NOW}
+
   }, {});
   Campaigns.associate = function(models) {
     // associations can be defined here
